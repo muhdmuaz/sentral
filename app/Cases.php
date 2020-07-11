172bc9411d88d356617af4acf39bcc6952622f5c
Muhammad Mu'az <muhdmuaz@gmail.com>
2020-07-11 22:41:32 +0800

    update cases model
    Signed-off-by: Muhammad Mu'az <muhdmuaz@gmail.com>

--- app/Cases.php
+++ app/Cases.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Cases extends Model
 {
     //
+    protected $fillable = ['title','date','remark','status'];
+
+    public function visitor(){
+        return $this->belongsTo('App\Vistor');
+    }
 }
